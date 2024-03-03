93c694a3b2739ffef4f9f2282dbe632f14ac9bf5
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-03-03 13:32:22 -0800

    /hello

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router, ServiceExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async {
+            Html("Hello <strong>Leah!</strong>")
+        })
+    );
+
+    // start server
+    let addr = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("-> LISTENING ON: {}", addr.local_addr().unwrap());
+    axum::serve(addr, routes_hello.into_make_service()).await.unwrap();
 }
