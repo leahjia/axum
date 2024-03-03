d1c583c68ac2a18abd94232b5492676a3b47e388
Leah Jia <99011487+leahjia@users.noreply.github.com>
2024-03-03 14:13:42 -0800

    take params directly from path

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use axum::{response::{Html, IntoResponse}, routing::get, Router};
+use axum::{extract::{Path, Query}, response::{Html, IntoResponse}, routing::get, Router};
+use serde::Deserialize;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     // take a path, make a route
-    let routes_hello = Router::new().route( "/hello", get(handler_hello));
+    let routes_hello = Router::new()
+        .route( "/hello", get(handler_hello))
+        .route( "/helloagain/:name", get(handler_helloagain));
 
     // start server
     let addr = TcpListener::bind("127.0.0.1:8080").await.unwrap();
@@ -12,8 +15,23 @@ async fn main() {
     axum::serve(addr, routes_hello.into_make_service()).await.unwrap();
 }
 
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>
+}
+
 // handler hello
-async fn handler_hello() -> impl IntoResponse {
-    println!("-> {:?} - handler_hello", "HANDLER");
-    Html("Hello <string>Leah!</string>")
+// e.g. `/hello?name=Jane`
+// side note - destructuring the Query struct using Query(params) 
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("-> {:?} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("Leah");
+    Html(format!("Hello <string>{name}</string>!!"))
+}
+
+// e.g. `/helloagain/Mike`
+async fn handler_helloagain(Path(name): Path<String>) -> impl IntoResponse {
+    println!("-> {:?} - handler_helloagain - {:?}", "HANDLER", name);
+    Html(format!("Hello <string>{name}</string>!!"))
 }
--- tests/dev.rs
+++ tests/dev.rs
@@ -7,6 +7,8 @@ async fn dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
     hc.do_get("/hello").await?.print().await?;
+    hc.do_get("/hello?name=Jane").await?.print().await?;
+    hc.do_get("/helloagain/Mike").await?.print().await?;
 
     Ok(())
 }
